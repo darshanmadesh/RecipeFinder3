f34e66e137f0f80c0a8f8ba76c7c718c4f88f070
Darshan Madesh <darshan.madesh@gmail.com>
2020-12-05 11:16:17 +0530

    Basic react setup and npm init

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+    return (
+        <div>
+            <Container>
+                <h1>MERN App</h1>
+            </Container>
+        </div>
+    );
+};
+
+export default App;
