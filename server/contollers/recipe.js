b33e49532b093836f9ec25080ec3fdddf8f6a164
Darshan Madesh <darshan.madesh@gmail.com>
2020-12-05 12:28:49 +0530

    setup backend for adding recipes

--- server/contollers/recipe.js
+++ server/contollers/recipe.js
@@ -1,5 +1,33 @@
+const Recipe = require("../models/recipe");
+
+const slugify = require("slugify");
+
 exports.create = (req, res) => {
-    res.json({
-        message: "Hello from controller",
+    const { name, instructions, user, image, cooktime, yields } = req.body;
+
+    const slug = slugify(name);
+
+    //Validate
+    switch (true) {
+        case !name:
+            return res.status(400).json({ error: "Name is required" });
+            break;
+        case !instructions:
+            return res.status(400).json({ error: "instructions is required" });
+            break;
+        case !cooktime:
+            return res.status(400).json({ error: "cook time is required" });
+            break;
+        case !yields:
+            return res.status(400).json({ error: "yields is required" });
+            break;
+    }
+    Recipe.create({ name, instructions, user, image, cooktime, yields, slug }, (err, recipe) => {
+        if (err) {
+            console.log(err);
+            res.status(400).json({ error: "Some Error" });
+        }
+
+        res.json(recipe);
     });
 };
--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -5,6 +5,6 @@ const router = express.Router();
 //Import controller methods
 const { create } = require("../contollers/recipe");
 
-router.get("/recipe", create);
+router.post("/recipe", create);
 
 module.exports = router;
